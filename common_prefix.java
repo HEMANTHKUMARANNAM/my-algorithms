7bc52d3b19c1146c4a2ccfea19958e4698e5dfc3
HEMANTH KUMAR <hemanthkumar312004@gmail.com>
2023-12-18 11:21:25 +0530

    add

--- common_prefix.java
+++ common_prefix.java
@@ -10,27 +10,53 @@ public class common_prefix
         System.out.print("Enter String Count : ");
         int c = sc.nextInt();
 
+        sc.nextLine();  // Consume newline left-over
+
         String[] array = new String[c];
 
         for (int i = 0; i < c; i++)
         {
             System.out.print("Enter String["+ (i+1) +"] : ");
-            array[i] = sc.next();
+            array[i] = sc.nextLine();
+        }
+
+        System.out.println("Common String = " + Common_prefix(array) );
+
+    }
+
+    public static String Common_prefix(String array[])
+    {
+        if(array.length ==0 )
+        {
+            return "";
+        }
+
+        if( array.length == 1 )
+        {
+            return array[0];
         }
 
         Arrays.sort(array, Comparator.comparing(s->s.length()));
 
+        int array_length = array.length;
+
         int len = array[0].length();
+
 
         for (int i = 0; i < len; i++)
         {
-            for(int j = 1; j < len; j++)
+            for(int j = 1; j < array_length; j++)
             {
-                if(  )
+                if(  array[0].charAt(i) != array[j].charAt(i)  )
+                {
+                    return array[0].substring(0,i);
+                }
             }
         }
 
-        System.out.println("Common String = " + array);
-
+        return null;
+    
     }
+
+
 }
